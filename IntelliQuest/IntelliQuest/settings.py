097bf8431326f57c4117184735e2b6f296b60f8d
Chhatrapathi Sivaji Lakkimsetty <58470174+chhatrapathisivaji@users.noreply.github.com>
2024-04-30 05:26:47 -0400

    Update settings.py

--- IntelliQuest/IntelliQuest/settings.py
+++ IntelliQuest/IntelliQuest/settings.py
@@ -115,6 +115,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
 }
 
 SIMPLE_JWT = {
